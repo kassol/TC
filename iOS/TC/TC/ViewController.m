422031b14b51f0a7ff2e675487c5a8924611ac84
kassol <kassol.zx@gmail.com>
2015-04-22 19:05:31 +0800

    Create iOS project.

---
+++ iOS/TC/TC/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TC
+//
+//  Created by 张旭 on 15/4/22.
+//  Copyright (c) 2015年 3lang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ iOS/TC/TC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TC
+//
+//  Created by 张旭 on 15/4/22.
+//  Copyright (c) 2015年 3lang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
