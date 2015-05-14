d94b37d83e104c5a5d8a4bdf9aef78b84b53c983
Kassol <kassol.zx@gmail.com>
2015-05-15 00:53:43 +0800

    Update201505150054
    1. Add label class.

---
+++ iOS/TC/TC/ProgressLabel.h
@@ -0,0 +1,21 @@
+//
+//  ProgressLabel.h
+//  TC
+//
+//  Created by kassol on 15/5/14.
+//  Copyright (c) 2015年 3lang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProgressLabel : UILabel
+
+- (void)start;
+
+- (void)pause;
+
+- (void)stop;
+
+- (void)resume;
+
+@end
---
+++ iOS/TC/TC/ProgressLabel.m
@@ -0,0 +1,38 @@
+//
+//  ProgressLabel.m
+//  TC
+//
+//  Created by kassol on 15/5/14.
+//  Copyright (c) 2015年 3lang. All rights reserved.
+//
+
+#import "ProgressLabel.h"
+
+@interface ProgressLabel ()
+
+@property (nonatomic, strong) NSTimer *timer;
+@property (nonatomic) float drawedTime;
+@property (nonatomic) float totalTime;
+@property (nonatomic) BOOL finished;
+
+@end
+
+@implementation ProgressLabel
+
+- (void)start {
+    
+}
+
+- (void)pause {
+    
+}
+
+- (void)stop {
+    
+}
+
+- (void)resume {
+    
+}
+
+@end
