5efe4f05ab527044835fc593f5588b85eb6ad834
Kassol <kassol.zx@gmail.com>
2015-05-14 13:25:24 +0800

    Update201505141325
    1. Draw the progress.

---
+++ iOS/TC/TC/ProgressView.h
@@ -0,0 +1,13 @@
+//
+//  ProgressView.h
+//  TC
+//
+//  Created by kassol on 15/5/14.
+//  Copyright (c) 2015年 3lang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProgressView : UIView
+
+@end
---
+++ iOS/TC/TC/ProgressView.m
@@ -0,0 +1,33 @@
+//
+//  ProgressView.m
+//  TC
+//
+//  Created by kassol on 15/5/14.
+//  Copyright (c) 2015年 3lang. All rights reserved.
+//
+
+#import "ProgressView.h"
+
+@implementation ProgressView
+
+- (void)drawRect:(CGRect)rect {
+    CGRect bounds = self.bounds;
+    
+    CGPoint center;
+    center.x = bounds.origin.x+bounds.size.width/2.0;
+    center.y = bounds.origin.y+bounds.size.height/2.0;
+    
+    float radius = bounds.size.width/2.0-10;
+    
+    UIBezierPath *path = [[UIBezierPath alloc] init];
+    
+    [path addArcWithCenter:center radius:radius startAngle:0.5*M_PI endAngle:2*M_PI clockwise:YES];
+    
+    path.lineWidth = 5.0;
+    
+    [[UIColor colorWithRed:55/255.0 green:101/255.0 blue:185/255.0 alpha:1.0] setStroke];
+    
+    [path stroke];
+}
+
+@end
