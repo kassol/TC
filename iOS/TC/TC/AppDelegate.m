e569db6271f2d7a8b7c38e80508871fc13f8fa3f
Kassol <kassol.zx@gmail.com>
2015-05-22 17:45:12 +0800

    Update201505221745
    1. Don't elapse time when the progress is pause.

--- iOS/TC/TC/AppDelegate.m
+++ iOS/TC/TC/AppDelegate.m
@@ -48,7 +48,9 @@
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     NSTimeInterval elapse = [[NSDate date] timeIntervalSinceDate:[ProgressInfo sharedProgressInfo].backgroundTimeStamp];
-    [[ProgressInfo sharedProgressInfo] elapse:elapse];
+    if ([[ProgressInfo sharedProgressInfo] isStarted] && ![[ProgressInfo sharedProgressInfo] isPaused]) {
+        [[ProgressInfo sharedProgressInfo] elapse:elapse];
+    }
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
